7617997844d3493acb8da6666ec0c237d319f92d
Maruo119 <work20250104@gmail.com>
2026-03-07 12:25:55 +0900

    Initial commit with project files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userRoutes = require('./routes/users');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
